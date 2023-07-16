9d5104156720b77a6cf5db7f9d5a6e8dc9fcff0b
Erik <erik.widell@hotmail.com>
2023-07-16 17:55:10 +0200

    Add project files.

---
+++ UniWeb/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using UniWeb.Data.Models;
+
+namespace UniWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options) 
+            : base(options)        
+        { 
+                   
+        }
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
---
+++ UniWeb/Services/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniWeb.Data;
+using UniWeb.Data.Models;
+
+namespace UniWeb.Services
+{
+    public class CustomerService
+    {
+        private IDbContextFactory<AppDbContext> _dbContextFactory;
+
+        public CustomerService(IDbContextFactory<AppDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                context.Customer.Add(customer);
+            }
+        }
+    }
+}
