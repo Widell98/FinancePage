1071a963ced82bba597fb3b152aaaf8fd9d57a44
widell98 <erik.widell@hotmail.com>
2023-07-16 18:18:12 +0200

    working database

--- UniWeb/Services/CustomerService.cs
+++ UniWeb/Services/CustomerService.cs
@@ -18,6 +18,32 @@ namespace UniWeb.Services
             using (var context = _dbContextFactory.CreateDbContext())
             {
                 context.Customer.Add(customer);
+                context.SaveChanges();
+            }
+        }
+
+        public Customer getCustomerByName(string name)
+        {
+            using var context = _dbContextFactory.CreateDbContext();
+            {
+                var customer = context.Customer.FirstOrDefault(x => x.Name == name);
+                return customer;
+            }
+        }
+
+        public void UpdateCustomer(string name, int age)
+        {
+            var customer = getCustomerByName(name);
+            if (customer == null)
+            {
+                throw new Exception("Customer does not exist. Cannot update");
+
+            }
+            customer.Age = age;
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                context.Update(customer);
+                context.SaveChanges();
             }
         }
     }
