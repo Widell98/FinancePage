5ec7668a8956389c69546f620cf64e8160ba3d26
widell98 <erik.widell@hotmail.com>
2023-07-29 13:00:46 +0200

    delete works

--- UniWeb/Data/Models/Stock.cs
+++ UniWeb/Data/Models/Stock.cs
@@ -7,9 +7,8 @@ namespace UniWeb.Data.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; }      
         public int Price { get; set; }
-
 
     }
 }
--- UniWeb/Services/StockServices.cs
+++ UniWeb/Services/StockServices.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using UniWeb.Data;
 using UniWeb.Data.Models;
 
@@ -6,6 +8,7 @@ namespace UniWeb.Services
 {
     public class StockServices
     {
+
         private IDbContextFactory<AppDbContext> _dbContextFactory;
 
         public StockServices(IDbContextFactory<AppDbContext> dbContextFactory)
@@ -13,13 +16,43 @@ namespace UniWeb.Services
             _dbContextFactory = dbContextFactory;
         }
 
-        public void AddStock(Stock stock)
+        public bool IsStockExists(string stockName)
         {
             using (var context = _dbContextFactory.CreateDbContext())
             {
-                context.Add(stock);
-                context.SaveChanges();
+                return context.Stocks.Any(s => s.Name == stockName);
             }
         }
+
+        public void AddStock(Stock stock)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {                            
+                context.Add(stock);
+                context.SaveChanges();                                        
+            }
+        }
+
+        public List<Stock> GetAllStocks() 
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                return context.Stocks.ToList();
+            }
+        }
+
+        public void DeleteStock(int stockId)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                var stockToDelete = context.Stocks.FirstOrDefault(s => s.Id == stockId);
+                if (stockToDelete != null)
+                {
+                    context.Stocks.Remove(stockToDelete);
+                    context.SaveChanges();
+                }
+            }
+        }
+
     }
 }
