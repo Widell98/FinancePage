c9aa3217c5e20590e32fc34c75ee2153c2acb6dc
widell98 <erik.widell@hotmail.com>
2024-03-24 15:36:04 +0100

    Added enum sector

--- UniWebServer/Data/Models/Stock.cs
+++ UniWebServer/Data/Models/Stock.cs
@@ -2,6 +2,24 @@
 
 namespace UniWeb.Data.Models
 {
+
+    public enum Sector
+    {
+        [Display(Name = "Tech")]
+        Tech,
+        [Display(Name = "Investment")]
+        Investment,
+        [Display(Name = "Bank")]
+        Bank,
+        [Display(Name = "Healthcare")]
+        Healthcare,
+        [Display(Name = "Energy")]
+        Energy,
+        [Display(Name = "Consumer Goods")]
+        ConsumerGoods,
+        [Display(Name = "Index")]
+        Index
+    }
     public class Stock
     {
         [Key]
@@ -9,8 +27,8 @@ namespace UniWeb.Data.Models
 
         public string? Name { get; set; }
 
-        public string? Description { get; set; }      
-
+        public string? Description { get; set; }     
+        
         public int Price { get; set; }
 
         public DateTime DateTime { get; set; }
@@ -18,6 +36,9 @@ namespace UniWeb.Data.Models
         public byte[]? ImageData { get; set; }
 
         public string? ImageType { get; set; }
+
+        [EnumDataType(typeof(Sector))]
+        public Sector sector { get; set; }
 
     }
 }
---
+++ UniWebServer/Migrations/20240324134943_addedEnumsToSTock.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedEnumsToSTock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "sector",
+                table: "Stocks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sector",
+                table: "Stocks");
+        }
+    }
+}
