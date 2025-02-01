8eca75f857b0e8de77aff406685c54de7069403b
erik <erik.widell@hotmail.com>
2025-02-01 16:33:29 +0100

    possible to save blogposts to database

--- UniWebServer/Data/Models/BlogPost.cs
+++ UniWebServer/Data/Models/BlogPost.cs
@@ -1,8 +1,11 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace UniWebServer.Data.Models
 {
     public class BlogPost
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
--- UniWebServer/Services/BlogService.cs
+++ UniWebServer/Services/BlogService.cs
@@ -1,21 +1,50 @@
-﻿using UniWebServer.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using UniWebServer.Data;
+using UniWebServer.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniWeb.Data;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace UniWebServer.Services
 {
     public class BlogService
     {
-        private List<BlogPost> posts = new();
+        private IDbContextFactory<AppDbContext> _dbContextFactory;
 
-        public List<BlogPost> GetPosts() => posts;
-
-        public void AddPost(BlogPost post) => posts.Insert(0, post); // Nyaste först
-
-        public void DeletePost(int postId)
+        public BlogService(IDbContextFactory<AppDbContext> dbContextFactory)
         {
-            var post = posts.FirstOrDefault(p => p.Id == postId);
-            if (post != null)
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<List<BlogPost>> GetPostsAsync()
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
             {
-                posts.Remove(post);
+                return await context.Blog.OrderByDescending(p => p.Date).ToListAsync();
+            }
+        }
+
+        public void AddPost(BlogPost post)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                context.Blog.Add(post);
+                context.SaveChanges();
+            }
+        }
+
+        public async Task DeletePostAsync(int postId)
+        {         
+            using(var context = _dbContextFactory.CreateDbContext())
+            {
+                var post = await context.Blog.FindAsync(postId);
+                if (post != null)
+                {
+                    context.Blog.Remove(post);
+                    await context.SaveChangesAsync();
+                }
             }
         }
     }
